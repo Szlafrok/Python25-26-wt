baf50b9ad4321582f457c0963e3e62bec57c027f
Szlafrok <polanski.piotrek04@gmail.com>
2026-03-10 18:25:56 +0100

    po lekcji 1

--- Lekcja01 - Wstep/projekt.py
+++ Lekcja01 - Wstep/projekt.py
@@ -1,2 +1,7 @@
 print("Hello World")
 print("Cześć, jestem Piotrek!")
+print("Mam 21 lat i lubię placki")
+
+print("A ty jak masz na imię?")
+user = input("Podaj imię: ")
+print("Witaj, " + user) # Tu wypisujemy wprowadzone dane

ad9691d6dce61773a05e569b1ca86bbc76d57a68
Szlafrok <polanski.piotrek04@gmail.com>
2026-03-17 17:41:35 +0100

    L2 aktualizacja

---
+++ Lekcja02 - Zmienne/projekt.py
@@ -0,0 +1,23 @@
+zmienna = 5
+zmienna = 7
+zmienna_2 = 10
+
+tekst = "hyhyhy ktokolwiekcokolwiekcoś"
+
+print(zmienna_2) # wypisanie wartości zmiennej
+
+tekst_1 = "abc"
+tekst_2 = "def"
+
+polaczone = tekst_1 + tekst_2
+
+print(polaczone)
+
+liczba_1 = 12
+liczba_2 = 12
+
+suma = liczba_1 + liczba_2
+
+print(suma)
+
+# print(tekst_1 + liczba_1) BŁAD

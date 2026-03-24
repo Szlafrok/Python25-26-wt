e7c6dc6a5cc45e7e21794c912f4cb481c89970bc
Szlafrok <polanski.piotrek04@gmail.com>
2026-03-24 18:10:52 +0100

    treść zadania

---
+++ Lekcja03 - Float i obliczenia/geometria.py
@@ -0,0 +1,9 @@
+# Proszę napisać program, który wczyta 2 liczby dziesiętne,
+# które są bokami prostokąta. Zmienne powinny nazywać się a
+# oraz b. 
+
+# Prosze napisać program, który na podstawie tych zmiennych wyznaczy
+# obwód tego prostokąta oraz jego pole.
+
+# Obwód prostokąta = suma wszystkich boków
+# Pole prostokąta = iloczyn wszystkich boków

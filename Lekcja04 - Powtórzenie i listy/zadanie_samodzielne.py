01febf8f0647b63ca53613208d9e003acb591308
Szlafrok <polanski.piotrek04@gmail.com>
2026-03-31 17:41:18 +0200

    rozwiązanie zadania samodzielnego

--- Lekcja04 - Powtórzenie i listy/zadanie_samodzielne.py
+++ Lekcja04 - Powtórzenie i listy/zadanie_samodzielne.py
@@ -2,11 +2,11 @@ wynik = 4 # 4
 wynik += 5 # 9
 wynik *= 8 # 72
 print(wynik)
-wynik %= 5
-wynik += 7
-wynik /= 3
-wynik *= 10
-wynik **= 2 
+wynik %= 5 # 2
+wynik += 7 # 9
+wynik /= 3 # 3
+wynik *= 10 # 30
+wynik **= 2 # 900
 
 # %= dzielenie z resztą: 5 % 2 -> 1
 # **= potęgowanie: a **= b -> a = a**b

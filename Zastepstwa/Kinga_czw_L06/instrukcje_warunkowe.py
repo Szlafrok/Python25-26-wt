7c48a1687a5c65b0d1825d29fbe2149a4539d7fa
Szlafrok <ppolanski@student.agh.edu.pl>
2026-04-02 16:30:23 +0200

    boolean

--- Zastepstwa/Kinga_czw_L06/instrukcje_warunkowe.py
+++ Zastepstwa/Kinga_czw_L06/instrukcje_warunkowe.py
@@ -1,7 +1,22 @@
-v = int(input("Podaj prędkość: "))
+# v = int(input("Podaj prędkość: "))
 
-if v > 140:
-    print("Jedziesz za szybko!")
-    print("Zwolnij")
+# if v > 140:
+#     print("Jedziesz za szybko!")
+#     print("Zwolnij")
 
-# Wykryj prędkość poniżej 30 km/h i nakaż użytkownikowi przyspieszenie
+# # Wykryj prędkość poniżej 30 km/h i nakaż użytkownikowi 
+# # przyspieszenie
+
+# if v < 30:
+#     print("Jedziesz za wolno!")
+#     print("Przyspiesz")
+
+print("------ ABRAKADABRA -------------------------")
+
+haslo = "kebab124"
+podane_haslo = input("Podaj tajne hasło: ")
+
+if haslo == podane_haslo:
+    print("Poprawne dane logowania")
+else:
+    print("Niepoprawne dane logowania!")

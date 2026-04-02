e0ae71c25b3e9570a1e8e387b8e8e93a3d278a1f
Szlafrok <ppolanski@student.agh.edu.pl>
2026-04-02 16:44:23 +0200

    relacyjne

--- Zastepstwa/Kinga_czw_L06/porownania.py
+++ Zastepstwa/Kinga_czw_L06/porownania.py
@@ -1,12 +1,19 @@
-print(5 > 4)
-print(5 < 4)
-print(5 > 5)
-print(5 >= 5)
-print(5 <= 5)
-print(5 != 5)
-print(5 != 4)
-print(5 == 5)
-print(5 == 4)
-print(5 == 2 + 3)
-print('test' == 'test')
-print('test' == 'test2')
+# Większe / mniejsze
+print(5 > 4) # True
+print(5 < 4) # False
+print(5 > 5) # False
+
+# Większe lub równe / mniejsze lub równe
+print(5 >= 5) # True
+print(5 <= 5) # True
+
+# Nierówne (różne)
+print(5 != 5) # False
+print(5 != 4) # True
+
+# Równe
+print(5 == 5) # True
+print(5 == 4) # False
+print(5 == 2 + 3) # True
+print('test' == 'test') # True
+print('test' == 'test2') # False

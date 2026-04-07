6afffcd24e334847bf5f66bdcf06e44d41cbe4c0
Szlafrok <polanski.piotrek04@gmail.com>
2026-04-07 17:57:52 +0200

    przed samodzielnym

--- Lekcja05 - For/petle.py
+++ Lekcja05 - For/petle.py
@@ -21,3 +21,27 @@ suma_cen = 0.0
 for c in ceny:
     suma_cen += c
 print(f"Ceny: {suma_cen}")
+
+print("----------------------------------")
+
+print(list(range(5))) # range(stop) -> liczby od 0 do stop, ALE bez stopu
+
+print(list(range(10)))
+
+print(list(range(2)))
+
+for i in range(6):
+    print(f"{i} HELOU :D")
+
+print("--------------------------")
+produkty = ["mysz", "płatki", "chleb", "zmywarka", "kebab"]
+ceny = [249.00, 7.50, 3.49, 1699.99, 10.25]
+
+# n - długość listy
+# indeksy listy: 0, 1, 2, ..., n-1
+# range(n):      0, 1, 2, ..., n-1
+
+n = len(produkty) # 5
+for i in range(n): # range(5): 0, 1, 2, 3, 4 -> PRZECHOWYWANE W ZMIENNEJ i
+    print(f"Indeks {i}")
+    print(f"{produkty[i]} - cena {ceny[i]} zł")

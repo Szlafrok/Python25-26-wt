45b298194515d7c7b6a5c3773b901339a21e0027
Szlafrok <polanski.piotrek04@gmail.com>
2026-04-21 17:43:08 +0200

    zad samodzielne

---
+++ Lekcja07 - Zagniezdzanie i powtorzenie/elify.py
@@ -0,0 +1,31 @@
+dzien = int(input())
+
+if dzien == 1:
+    print("poniedziałek")
+elif dzien == 2:
+    print("wtorek")
+elif dzien == 3:
+    print("środa")
+elif dzien == 4:
+    print("czwartek")
+elif dzien == 5:
+    print("piątek")
+elif dzien == 6:
+    print("sobota")
+else:
+    print("niedziela")
+
+
+# jeżeli dzień = 1 to wypisz poniedziałek
+# w przeciwnym razie jeżeli dzień = 2 to wypisz wtorek
+# w przeciwnym razie jeżeli dzień = 3 to wypisz środa
+# w przeciwnym razie jeżeli dzień = 4 to wypisz czwartek
+# w przeciwnym razie jeżeli dzień = 5 to wypisz piątek
+# w przeciwnym razie jeżeli dzień = 6 to wypisz sobota
+# w przeciwnym razie wypisz niedziela
+
+
+
+# Zadanie samodzielne
+# Proszę do zmiennej 'kolor' wpisać literę "R" (red - czerwony), "Y" (yellow - żółty), "G" (green - zielony)
+# Napisz program, który na podstawie zmiennej kolor powie, czy trzeba się zatrzymać, przygotować czy ruszać.

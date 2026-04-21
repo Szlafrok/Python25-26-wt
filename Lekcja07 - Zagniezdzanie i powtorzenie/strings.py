aa5075a780631148faed471804b62c50e3cecb9d
Szlafrok <polanski.piotrek04@gmail.com>
2026-04-21 18:27:00 +0200

    po lekcji

---
+++ Lekcja07 - Zagniezdzanie i powtorzenie/strings.py
@@ -0,0 +1,24 @@
+tekst = "MANAAM"
+tekst2 = "umpalumpas"
+tekst3 = "AmperCola123"
+
+
+# upper
+print(tekst.upper())
+print(tekst2.upper())
+print(tekst3.upper())
+
+# lower
+print(tekst.lower())
+print(tekst2.lower())
+print(tekst3.lower())
+
+# isupper
+print(tekst.isupper()) # tak
+print(tekst2.isupper()) # nie
+print(tekst3.isupper()) # nie
+
+# islower
+print(tekst.islower()) # nie
+print(tekst2.islower()) # tak
+print(tekst3.islower()) # nie

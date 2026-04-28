618ad7f2fc26db14ec8233c23abb7a34ee89662d
Szlafrok <polanski.piotrek04@gmail.com>
2026-04-28 17:15:24 +0200

    zadanie samodzielne

---
+++ Lekcja08 - While/zadanko.py
@@ -0,0 +1,9 @@
+HASLO = "kraboburger"
+LOGIN = "szlafrok"
+
+# Prosze napisać program, który wczytuje od użytkownika login i hasło
+# a następnie wypisuje OK, jesli dane logowania (hasło i login) są poprawne
+# lub "BŁĄD", jeśli nie są poprawne.
+
+# Należy zapisać wartości funkcją input() do zmiennych i wykorzystać 
+# słowo IF wraz z operatorami logicznymi.

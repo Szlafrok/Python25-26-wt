6176b4571b93e1b8832ecc169fc58c0131c3038d
Szlafrok <polanski.piotrek04@gmail.com>
2026-04-28 17:07:42 +0200

    lekcja 8

---
+++ Lekcja08 - While/pokazy.py
@@ -0,0 +1,20 @@
+lista = [1, 2, 5, 4, 3, 3]
+
+print(sum(lista))
+
+print(min(lista))
+print(max(lista))
+
+print(lista.count(3))
+
+
+tekst = "umpalumpas"
+
+for litera in tekst:
+    print("--" + litera + "--") # dodawanie stringów - KONKATENACJA
+
+x = 4
+if x > 3: # zagnieżdżenie
+    print("większy od 3")
+    if x % 2 == 0:
+        print("parzysty")

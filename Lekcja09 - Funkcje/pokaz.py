6126e0a23fb7bcbd3c655884baaec6f6d351f30d
Szlafrok <polanski.piotrek04@gmail.com>
2026-05-05 17:13:38 +0200

    zadanie samodzielne 1

---
+++ Lekcja09 - Funkcje/pokaz.py
@@ -0,0 +1,19 @@
+def napompuj_kolo(): # DEFINICJA FUNKCJI
+    print("Wziąć pompkę")
+    print("Odkręcić zatyczkę")
+    print("Podpiąć pompkę")
+    for i in range(5):
+        print("Pompujemy koło")
+    print("Koło napompowane")
+
+napompuj_kolo() # WYWOŁANIE FUNKCJI
+print("-----")
+napompuj_kolo()
+
+
+# Proszę napisać definicję funkcji przedstaw_sie(), w ramach
+# której wypiszesz swoje imię, wiek i hobby w trzech osobnych
+# printach.
+
+# Następnie proszę napisać wywołanie tej funkcji i sprawdzić
+# działanie programu.
---
+++ Lekcja09 - Funkcje/powtorka.py
@@ -0,0 +1,8 @@
+x = 5
+
+while x < 10:
+    print("hehe")
+    x = 20
+    continue
+    x = 5
+    print("juhu")

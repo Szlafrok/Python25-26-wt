27c354f437a4dc1dd6a8b696ff88867565802367
Szlafrok <polanski.piotrek04@gmail.com>
2026-05-05 18:25:10 +0200

    po zajęciach

---
+++ Lekcja09 - Funkcje/domylne.py
@@ -0,0 +1,16 @@
+def iloczyn(a = 2, b = 2):
+    return a * b
+
+print(iloczyn())
+
+print(iloczyn(5)) # 10
+
+print(iloczyn(5, 3)) # 15
+
+# print(iloczyn(5, 3, 1)) BŁĄD
+
+# 1. Argumenty domyślne są Z PRAWEJ STRONY
+
+
+# def funkcja(a = 5, b) - NIEDOPUSZCZALNE
+# funkcja(3)
---
+++ Lekcja09 - Funkcje/zwracanie.py
@@ -0,0 +1,16 @@
+# input()
+# round()
+
+# from math import sqrt
+
+# sqrt()
+
+def prostokat(a, b):
+    print("Przeliczanie...")
+    return a * b
+    print("ups") # nigdy się nie wydarzy
+
+wynik = prostokat(3, 4)
+
+print(f"Pole prostokąta to {wynik}")
+print(f"Podwojone pole prostokąta to {wynik * 2}")

4d12849af9e15c54d3a476dadec7997731df7ec3
Szlafrok <polanski.piotrek04@gmail.com>
2026-06-02 18:10:52 +0200

    samodzielne 2

---
+++ Lekcja13 - Turtle 2/zadanie_samodzielne_2.py
@@ -0,0 +1,18 @@
+# Proszę, korzystając z poznanych dziś poleceń, narysować kwadrat
+# umieszczając w nim na wierzchołkach odpowiednio cztery kształty:
+
+# - arrow
+# - turtle
+# - square
+# - triangle
+
+# następnie należy ukryć żółwia poleceniem hide() 
+
+# KWADRAT NALEZY TWORZYĆ BEZ POLECEŃ FORWARD ANI BACK - TYLKO GOTO()
+# BAZA - (+++)
+
+
+
+# DODATKOWE (++)
+# Proszę zaprogramować stemplowanie tak, aby wszystkie cztery figury
+# były skierowane czubkami do środka kwadratu

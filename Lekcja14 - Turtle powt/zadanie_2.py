716d4cf5fc0fb7ccadfe3201f9a10b58b9c24930
Szlafrok <polanski.piotrek04@gmail.com>
2026-06-09 18:03:07 +0200

    treść zadania 2

--- Lekcja14 - Turtle powt/zadanie_2.py
+++ Lekcja14 - Turtle powt/zadanie_2.py
@@ -4,10 +4,20 @@ wspolrzedne = [(0, 0), (-100, -100), (-100, 100), (100, -100), (100, 100)]
 
 def kwadrat(x, y, kolor): # zbuduje kwadrat o boku 30 i wypełni go kolorem
     pass
+    # pętla
+    # goto
+    # up
+    # down
+    # forward
+    # left
+
+    # fillcolor
+    # beginfill
+    # endfill
 
 for i in range(5):
 
-    print(wspolrzedne[i])
+    print(wspolrzedne[i], kolory[i])
 
     x = wspolrzedne[i][0]
     y = wspolrzedne[i][1]

690635323ff3c9f7c00e8896a9076bf4551870a8
Szlafrok <polanski.piotrek04@gmail.com>
2026-06-16 18:01:03 +0200

    treść zadania

---
+++ Lekcja15 - Obiektowe/zadanie_samodzielne.py
@@ -0,0 +1,11 @@
+# Proszę napisać klasę Gracz. Każdy gracz ma:
+
+# CECHY
+# - nazwę użytkownika (username)
+# - poziom zdrowia (health)
+# - siła (power)
+
+# METODY
+# - upadek(self): zadaje 5 obrażeń od upadku i pisze "AUA"
+# - ustaw_sile(self, sila): SETTER siły (ustawia siłę gracza)
+# - pozyskaj_sile(self): GETTER siły (zwraca siłę gracza)

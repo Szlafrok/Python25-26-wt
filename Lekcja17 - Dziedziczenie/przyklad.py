07c3bfd6fd48339fc9914e7900ce01668ff05a52
Szlafrok <polanski.piotrek04@gmail.com>
2026-06-30 17:38:23 +0200

    dziedziczenie

--- Lekcja17 - Dziedziczenie/przyklad.py
+++ Lekcja17 - Dziedziczenie/przyklad.py
@@ -1,4 +1,4 @@
-class Miecz():
+class Katana():
     def __init__(self, wlasciciel, obrazenia):
         self.wlasciciel = wlasciciel
         self.obrazenia = obrazenia
@@ -7,21 +7,7 @@ class Miecz():
         print(f"Należę do {self.wlasciciel} i zadaję {self.obrazenia} obrażeń.")
 
     def szpanuj(self):
-        print("Jestem mieczem! Tak po prostu")
-
-class Katana(Miecz):
-    def __init__(self, wlasciciel, obrazenia):
-        super().__init__(wlasciciel, obrazenia) # odwołanie do konsturktora klasy wyższej
-
-    # metoda przedstaw_sie jest taka sama, więc nie muszę jej powtarzać
-
-    def szpanuj(self): # nadpisanie metody
         print("Jestem japońskim mieczem samuraja!")
-
-katana = Katana("Piotrek", 10)
-katana.przedstaw_sie()
-katana.szpanuj()
-
 
 class Szabla():
     def __init__(self, wlasciciel, obrazenia):

d910a7c0a35725599803cbf61d9115faea178af7
Szlafrok <polanski.piotrek04@gmail.com>
2026-09-15 18:23:21 +0200

    po lekcji 2

--- Lekcja02_Zmienne/projekt.py
+++ Lekcja02_Zmienne/projekt.py
@@ -11,3 +11,25 @@ print(wiek + 5) # 55
 print(wiek / 5) # 10
 x = 20
 print(wiek) # 50
+
+# ---------------------
+
+print(5 ** 2) # 25 - 
+print(147 // 10) # 14 - 
+print(147 % 10) # 7 -
+
+# ---------------------
+
+calkowita = 10      # int (liczba całkowita)
+dziesietna = 10.0   # float (liczba dziesiętna / zmiennoprzecinkowa)
+tekst = "10"        # str (string - ciąg znaków)
+
+print("konka" + "tenacja") # konkatenacja - POŁĄCZENIE CIĄGÓW
+print("uga " * 5) # powtarzanie tekstu ileś razy
+print("a" * 5 + " " + "b" * 10)
+
+# Proszę z wykorzystaniem mnożenia i dodawania stringów napisać
+# print, który wypisze tekst:
+
+# aaaaaaa kotki aaaaaaa
+print("a" *7 + " kotki " + "a" *7)

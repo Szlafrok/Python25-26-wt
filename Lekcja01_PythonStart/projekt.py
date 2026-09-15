f830c9ff4eec28fc231263c6bf23c19b91040d36
Szlafrok <polanski.piotrek04@gmail.com>
2026-09-15 17:24:16 +0200

    initial commit

---
+++ Lekcja01_PythonStart/projekt.py
@@ -0,0 +1,2 @@
+print("Hello world")
+print("Nazywam się Grześ")
---
+++ Lekcja02_Zmienne/projekt.py
@@ -0,0 +1,13 @@
+print("Witam ponownie w mojej kuchni")
+
+wiek = 22
+print(wiek) # 22
+print("wiek") # wiek nam strzelać nie kazano wstąpiłem na działo coś tam coś tam lubie placki
+
+print(wiek * 10) # 220
+print(wiek - 22) # 0
+wiek = 50
+print(wiek + 5) # 55
+print(wiek / 5) # 10
+x = 20
+print(wiek) # 50

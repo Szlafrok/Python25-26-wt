30e44c19a5ef5efb285ce4e8eae4ca50fa573395
Szlafrok <polanski.piotrek04@gmail.com>
2026-09-22 17:27:17 +0200

    do przerwy

---
+++ Lekcja03_Obliczenia/projekt.py
@@ -0,0 +1,23 @@
+liczba = 10
+liczba_2 = 15
+
+tekst = "10"
+tekst_2 = "15"
+
+print(liczba + liczba_2) # 25
+print(tekst + tekst_2) # 1015
+# print(liczba + tekst) # BŁĄD
+
+int() 
+float() 
+str() 
+
+print( type("123") ) # <class 'str'>
+print( type(123) ) # <class 'int'>
+print( type(5.0) ) # <class 'float'>
+
+wartosc = int(12.4)
+print(wartosc)
+
+wartosc = float(2)
+print(wartosc)
